f70a4653608969e30eb12b53488559bb207d7007
SakitoHirao <sakihira.s15@icloud.com>
2025-04-28 17:14:23 +0900

    This is first BMIcalc App

--- BMICalApp/ViewController.swift
+++ BMICalApp/ViewController.swift
@@ -9,11 +9,43 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var HeightTextField: UITextField!
+    @IBOutlet weak var WeightTextField: UITextField!
+    @IBOutlet weak var BMILabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        HeightTextField.placeholder = "身長を入力してください"
+        WeightTextField.placeholder = "体重を入力してください"
+        
+    }
+    
+    
+    @IBAction func calcButtonAction(_ sender: Any) {
+        let DoubleH = Double(HeightTextField.text!)
+        let DoubleW = Double(WeightTextField.text!)
+        BMILabel.text = calBMI(height: DoubleH!, weight: DoubleW!)
+    }
+    
+    func calBMI(height: Double, weight: Double) -> String {
+        
+        let h = height/100
+        let bmi = weight / (h * h)
+        var result: String
+        if bmi < 18.5 {
+            result = "低体重"
+        } else if bmi < 25 {
+            result = "正常体重"
+        } else if bmi < 30 {
+            result = "過体重"
+        } else {
+            result = "肥満"
+        }
+        return result.description
     }
 
 
+    
 }
 
