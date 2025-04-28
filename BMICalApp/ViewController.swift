69ea5f1c246ce1fe97529123fce689e4c6cb0946
SakitoHirao <sakihira.s15@icloud.com>
2025-04-28 16:04:31 +0900

    Initial Commit

---
+++ BMICalApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BMICalApp
+//
+//  Created by 平尾彩樹人 on 2025/04/28.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ BMICalAppTests/BMICalAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  BMICalAppTests.swift
+//  BMICalAppTests
+//
+//  Created by 平尾彩樹人 on 2025/04/28.
+//
+
+import Testing
+@testable import BMICalApp
+
+struct BMICalAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
